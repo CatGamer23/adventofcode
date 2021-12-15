c2f788aaa06b5f1a35986c004b65072de77d9cc8
CatGamer23 <lfiller23@gmail.com>
2021-12-15 18:02:44 -0500

    added 2021 day 01 and 02, and 02 art 2

--- 2021/01.py
+++ 2021/01.py
@@ -1,5 +1,10 @@
 def part1(data):
-  return None
+  count, previous = 0, 0
+  for i in range(len(data)):
+    if int(data[i]) > previous:
+      count += 1
+    previous = int(data[i])
+  return count - 1
 
 
 def part2(data):
--- 2021/02.py
+++ 2021/02.py
@@ -1,6 +1,23 @@
 def part1(data):
-  return None
+  pos, depth = 0, 0
+  for instruction in data:
+    if instruction.startswith('forward'):
+      pos += int(instruction.split(' ')[1])
+    elif instruction.startswith('up'):
+      depth -= int(instruction.split(' ')[1])
+    elif instruction.startswith('down'):
+      depth += int(instruction.split(' ')[1])
+  return pos * depth
 
 
 def part2(data):
-  return None
+  pos, depth, aim = 0, 0, 0
+  for instruction in data:
+    if instruction.startswith('forward'):
+      pos += int(instruction.split(' ')[1])
+      depth += aim * int(instruction.split(' ')[1])
+    elif instruction.startswith('up'):
+      aim -= int(instruction.split(' ')[1])
+    elif instruction.startswith('down'):
+      aim += int(instruction.split(' ')[1])
+  return pos * depth
