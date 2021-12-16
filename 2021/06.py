e18bb7530cb5cb8e23c7e99f357987586f09b388
CatGamer23 <lfiller23@gmail.com>
2021-12-16 09:08:48 -0500

    fixed

--- 2021/06.py
+++ 2021/06.py
@@ -1,6 +1,13 @@
 def part1(data):
-  return None
-
+  laternFishes = data[0]
+  for _ in range(1):
+    for fish in laternFishes.split(','):
+      fish = int(fish)
+      if fish == 0:
+        laternFishes += ",8"
+      else:
+        fish -= 1
+  return len(laternFishes.split(','))
 
 def part2(data):
   return None
