59f3591113baafee097935900a4bdf38521831f9
CatGamer23 <lfiller23@gmail.com>
2021-12-16 08:23:55 -0500

    updated

--- 2021/03.py
+++ 2021/03.py
@@ -1,14 +1,5 @@
 def part1(data):
-  counts = {"first": 0, "second": 0, "third": 0, "fourth": 0, "fifth": 0, "sixth": 0,
-            "seventh": 0, "eighth": 0, "ninth": 0, "tenth": 0, "eleventh": 0, "twelfth": 0}
-  for line in data:
-    loopCount = 0
-    lineList = [char for char in line]
-    for char in lineList:
-      if char == 1:
-        counts[counts[loopCount]] += 1
-      loopCount += 1
-  return counts
+  return None
 
 
 def part2(data):
