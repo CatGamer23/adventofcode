6d1e89e35f3b78ce660668f2f1688230f3dcc244
CatGamer23 <lfiller23@gmail.com>
2023-01-16 19:25:09 -0500

    Day 03

--- 2022/03.py
+++ 2022/03.py
@@ -1,6 +1,21 @@
 def part1(data):
-  return None
+  priority = 0
+  for line in data:
+    middle_index = len(line) // 2
+    common_items_set = set(line[:middle_index]).intersection(line[middle_index:])
+    common_item = common_items_set.pop()
+    ascii_start = 96 if common_item.islower() else 38
+    priority += ord(common_item) - ascii_start
+  return str(priority)
 
 
 def part2(data):
-  return None
+  groups = [[data[i], data[i + 1], data[i + 2]]
+            for i, _ in enumerate(data[:-2]) if i % 3 == 0]
+  priority = 0
+  for group in groups:
+    common_items_set = set(group[0]).intersection(group[1], group[2])
+    common_item = common_items_set.pop()
+    ascii_start = 96 if common_item.islower() else 38
+    priority += ord(common_item) - ascii_start
+  return str(priority)
