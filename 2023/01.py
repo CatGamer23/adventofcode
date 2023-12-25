4501d0d82564e70ed6e6bfc19ac248f3156e5549
CatGamer23 <lfiller23@gmail.com>
2023-12-25 00:22:04 -0500

    Added day 25

--- 2023/01.py
+++ 2023/01.py
@@ -1,4 +1,8 @@
 def part1(data):
+  for line in data:
+    # using regex, match the first number in variable line
+    # using regex, match the second number in variable line
+    pass
   return None
 
 
