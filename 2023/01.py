c43dca59bbd623f10defab469e70289de579f3c7
CatGamer23 <lfiller23@gmail.com>
2024-03-05 11:44:52 -0500

    Added part 2

--- 2023/01.py
+++ 2023/01.py
@@ -11,8 +11,14 @@ def part1(data):
 def part2(data):
   conversionTable = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
   total = 0
-  # add together the spelled out numbers in the data
   for line in data:
-    num = conversionTable[''.join(char for char in line if char.isalpha())]
-    print(num)
+    finalnum = ""
+    for char in line:
+      letter = ''.join(char)
+      if letter in conversionTable:
+        finalnum += conversionTable[letter]
+      if char.isdigit():
+        finalnum += char
+    print(finalnum)
+    # total += int(str(finalnum[0]) + str(finalnum[-1]))
   return total
