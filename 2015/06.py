dc5a2576dde8fbc0071d0563972ceb1b4661789b
CatGamer23 <lfiller23@gmail.com>
2024-12-05 03:56:24 -0500

    Added part 1 of day 6

--- 2015/06.py
+++ 2015/06.py
@@ -1,6 +1,22 @@
-def part1(data: list[str]) -> str | int | float | None:
-  return None
+import numpy as np
 
+def part1(data: list[str]) -> str | int | float | None:
+  grid: np.ndarray = np.zeros((1000, 1000), dtype=int)
+
+  for line in data:
+    command: list[str] = line.split(' ')
+    action: str = command[1] if command[0] == 'turn' else 'toggle'
+    start: tuple[int, ...] = tuple(map(int, command[-3].split(',')))
+    end: tuple[int, ...] = tuple(map(int, command[-1].split(',')))
+
+    if action == 'on':
+      grid[start[0]:end[0] + 1, start[1]:end[1] + 1] = 1
+    elif action == 'off':
+      grid[start[0]:end[0] + 1, start[1]:end[1] + 1] = 0
+    elif action == 'toggle':
+      grid[start[0]:end[0] + 1, start[1]:end[1] + 1] ^= 1
+
+  return int(np.sum(grid))
 
 def part2(data: list[str]) -> str | int | float | None:
   return None
