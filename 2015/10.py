70f0fa4ce1b9fb1fd33761828b3ef227ad03c766
CatGamer23 <lfiller23@gmail.com>
2024-12-09 16:29:39 -0500

    Updated with faster computation

--- 2015/10.py
+++ 2015/10.py
@@ -1,30 +1,27 @@
+import itertools
+
 def part1(data: list[str]) -> str | int | float | None:
-  expanded_input = list(data[0])
+  expanded_input = data[0]
 
   for _ in range(40):
-    i = 0
-    while i < len(expanded_input):
-      count = 1
-      while i + count < len(expanded_input) and expanded_input[i] == expanded_input[i + count]:
-        count += 1
-
-      expanded_input[i:i + count] = list(str(count) + expanded_input[i])
-      i += len(str(count)) + 1
+    new_expanded_input = []
+    for digit, group in itertools.groupby(expanded_input):
+      # print(digit, str(group))
+      count = len(list(group))
+      new_expanded_input.append(str(count) + digit)
+    expanded_input = ''.join(new_expanded_input)
 
   return len(expanded_input)
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  expanded_input = list(data[0])
+  expanded_input = data[0]
 
   for _ in range(50):
-    i = 0
-    while i < len(expanded_input):
-      count = 1
-      while i + count < len(expanded_input) and expanded_input[i] == expanded_input[i + count]:
-        count += 1
-
-      expanded_input[i:i + count] = list(str(count) + expanded_input[i])
-      i += len(str(count)) + 1
+    new_expanded_input = []
+    for digit, group in itertools.groupby(expanded_input):
+      count = len(list(group))
+      new_expanded_input.append(str(count) + digit)
+    expanded_input = ''.join(new_expanded_input)
 
   return len(expanded_input)
