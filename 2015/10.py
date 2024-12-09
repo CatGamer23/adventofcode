396639e6fda824f3c1a7232af940aa6099f527dc
CatGamer23 <lfiller23@gmail.com>
2024-12-09 15:52:16 -0500

    part 2, both are soooo slow, probs O(n^2), and needs to be updated sometime

--- 2015/10.py
+++ 2015/10.py
@@ -15,4 +15,16 @@ def part1(data: list[str]) -> str | int | float | None:
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  return None
+  expanded_input = list(data[0])
+
+  for _ in range(50):
+    i = 0
+    while i < len(expanded_input):
+      count = 1
+      while i + count < len(expanded_input) and expanded_input[i] == expanded_input[i + count]:
+        count += 1
+
+      expanded_input[i:i + count] = list(str(count) + expanded_input[i])
+      i += len(str(count)) + 1
+
+  return len(expanded_input)
