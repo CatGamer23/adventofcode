2ae3347eb095d995b8d529deffcf06c8c9c178a9
CatGamer23 <lfiller23@gmail.com>
2024-12-09 00:59:33 -0500

    added part1

--- 2015/08.py
+++ 2015/08.py
@@ -1,5 +1,13 @@
+from rich import print
 def part1(data: list[str]) -> str | int | float | None:
-  return None
+  total_characters: int = 0
+  total_interpreted: int = 0
+
+  for line in data:
+    total_characters += len(line)
+    total_interpreted += len(line.encode('utf-8').decode('unicode_escape')) - 2
+
+  return total_characters - total_interpreted
 
 
 def part2(data: list[str]) -> str | int | float | None:
