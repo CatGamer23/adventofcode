0de8f5c48d8680916d161a80dbeb50cd7f8cd656
CatGamer23 <lfiller23@gmail.com>
2024-12-09 16:41:54 -0500

    used better names

--- 2015/10.py
+++ 2015/10.py
@@ -1,27 +1,27 @@
 import itertools
 
+
 def part1(data: list[str]) -> str | int | float | None:
-  expanded_input = data[0]
+  sequence = data[0]
 
   for _ in range(40):
-    new_expanded_input = []
-    for digit, group in itertools.groupby(expanded_input):
-      # print(digit, str(group))
-      count = len(list(group))
-      new_expanded_input.append(str(count) + digit)
-    expanded_input = ''.join(new_expanded_input)
+    next_sequence = []
+    for digit, group in itertools.groupby(sequence):
+      group_length = len(list(group))
+      next_sequence.append(str(group_length) + digit)
+    sequence = ''.join(next_sequence)
 
-  return len(expanded_input)
+  return len(sequence)
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  expanded_input = data[0]
+  sequence = data[0]
 
   for _ in range(50):
-    new_expanded_input = []
-    for digit, group in itertools.groupby(expanded_input):
-      count = len(list(group))
-      new_expanded_input.append(str(count) + digit)
-    expanded_input = ''.join(new_expanded_input)
+    next_sequence = []
+    for digit, group in itertools.groupby(sequence):
+      group_length = len(list(group))
+      next_sequence.append(str(group_length) + digit)
+    sequence = ''.join(next_sequence)
 
-  return len(expanded_input)
+  return len(sequence)
