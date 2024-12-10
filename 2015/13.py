3ce38c4753e87edd2904b3f433de567f3da28d46
CatGamer23 <lfiller23@gmail.com>
2024-12-10 15:16:04 -0500

    Add part 2

--- 2015/13.py
+++ 2015/13.py
@@ -1,3 +1,4 @@
+from rich import print
 import itertools
 
 
@@ -29,4 +30,30 @@ def part1(data: list[str]) -> str | int | float | None:
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  return None
+  people_cache: dict[str, dict[str, int]] = {'Me': {}}
+
+  for line in data:
+    person1: str
+    gain_or_lose: str
+    amount: str
+    person2: str
+    person1, _, gain_or_lose, amount, _, _, _, _, _, _, person2 = line.replace('.', '').split(' ')  # noqa
+
+    # Add person to the 'Me' cache with a happiness of 0
+    people_cache['Me'][person1] = 0
+
+    # Convert the amount to an integer and make it negative if the person is losing happiness
+    calculated_amount: int = int(amount) if gain_or_lose == 'gain' else -int(amount)  # noqa
+
+    # Add the person to the cache if they don't exist, along with the other person and the amount of happiness
+    people_cache.setdefault(person1, {'Me': 0})[person2] = calculated_amount
+
+  biggest_change: int = 0
+  for perm in itertools.permutations(people_cache.keys()):
+    happiness: int = 0
+    for i in range(len(perm)):
+      happiness += people_cache[perm[i]][perm[(i + 1) % len(perm)]]
+      happiness += people_cache[perm[i]][perm[(i - 1) % len(perm)]]
+    biggest_change = max(biggest_change, happiness)
+
+  return biggest_change
