1614bd79efc6ed0b298fb50ce146b2d7b735b57b
CatGamer23 <lfiller23@gmail.com>
2024-12-10 01:09:08 -0500

    Addded parts 1&2 (hate how part2 gets the answer :( )

--- 2015/11.py
+++ 2015/11.py
@@ -1,6 +1,97 @@
+from string import ascii_lowercase
+
+
+def from_decimal(number: int, base: int = 26) -> str:
+  digits: list[str] = []
+  while number > 0:
+    number -= 1
+    number, remainder = divmod(number, base)
+    digits.append(chr(remainder + ord('a')))
+  return ''.join(digits[::-1])
+
+
+def to_decimal(string: str, base: int = 26) -> int:
+  result: int = 0
+  for char in string:
+    result = result * base + (ord(char) - ord('a') + 1)
+  return result
+
+
+# Rules:
+# 1. It does not contain the letters i, o, or l.
+# 2. It contains the straight increasing three letters.
+# 3. It contains at least two different, non-overlapping pairs of letters.
 def part1(data: list[str]) -> str | int | float | None:
-  return None
+  data_as_int: int = to_decimal(data[0])
+
+  invalid_password: bool = True
+  while invalid_password:
+    data_as_int += 1
+    password: str = from_decimal(data_as_int)
+
+    # Rule 1
+    if 'i' in password or 'o' in password or 'l' in password:
+      continue
+
+    # Rule 2
+    three_in_a_row: bool = any(
+      password[i:i + 3] in ascii_lowercase
+      for i in range(len(password) - 2)
+    )
+
+    if not three_in_a_row:
+      continue
+
+    # Rule 3
+    pairs: int = 0
+    i: int = 0
+    while i < len(password) - 1:
+      if password[i] == password[i + 1]:
+        pairs += 1
+        i += 2
+      else:
+        i += 1
+
+    if pairs < 2:
+      continue
+
+    invalid_password = False
+  return from_decimal(data_as_int)
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  return None
+  data_as_int: int = to_decimal(str(part1(data)))
+
+  invalid_password: bool = True
+  while invalid_password:
+    data_as_int += 1
+    password: str = from_decimal(data_as_int)
+
+    # Rule 1
+    if 'i' in password or 'o' in password or 'l' in password:
+      continue
+
+    # Rule 2
+    three_in_a_row: bool = any(
+      password[i:i + 3] in ascii_lowercase
+      for i in range(len(password) - 2)
+    )
+
+    if not three_in_a_row:
+      continue
+
+    # Rule 3
+    pairs: int = 0
+    i: int = 0
+    while i < len(password) - 1:
+      if password[i] == password[i + 1]:
+        pairs += 1
+        i += 2
+      else:
+        i += 1
+
+    if pairs < 2:
+      continue
+
+    invalid_password = False
+  return from_decimal(data_as_int)
