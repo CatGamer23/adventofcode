a52d2a207b83501595e8025fd3fe0f9810fe7a6b
CatGamer23 <lfiller23@gmail.com>
2024-12-10 01:30:28 -0500

    part1 & 2

--- 2015/12.py
+++ 2015/12.py
@@ -1,6 +1,27 @@
+import json
+
+
+def find_numbers(obj: dict, ignored_property: str | None = None) -> int:
+  if isinstance(obj, int):
+    return obj
+  if isinstance(obj, list):
+    return sum(find_numbers(i, ignored_property) for i in obj)
+  if isinstance(obj, dict):
+    if ignored_property in obj.values():
+      return 0
+    return sum(find_numbers(i, ignored_property) for i in obj.values())
+  return 0
+
+
 def part1(data: list[str]) -> str | int | float | None:
-  return None
+  parsed_json_data: dict = json.loads(''.join(data))
+
+  # Recursively search for numbers
+  return find_numbers(parsed_json_data)
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  return None
+  parsed_json_data: dict = json.loads(''.join(data))
+
+  # Recursively search for numbers, ignoring the property "red"
+  return find_numbers(parsed_json_data, "red")
