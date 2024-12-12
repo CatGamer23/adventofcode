ab6a17aca9b3b050fbd034c0233714bae25d5d8a
CatGamer23 <lfiller23@gmail.com>
2024-12-11 22:29:35 -0500

    added new days

--- 2015/15.py
+++ 2015/15.py
@@ -1,4 +1,29 @@
+from rich import print
+import itertools
+
+
 def part1(data: list[str]) -> str | int | float | None:
+  ingredients: dict[str, dict[str, int]] = {}
+
+  for line in data:
+    parts: list[str] = line.replace(',', '').replace(':', '').split(' ')
+    name, _, capacity, _, durability, _, flavor, _, texture, _, calories = parts
+
+    ingredients[name] = {
+      'capacity': int(capacity),
+      'durability': int(durability),
+      'flavor': int(flavor),
+      'texture': int(texture),
+      'calories': int(calories)
+    }
+
+
+  print(ingredients)
+  max_score: int = 0
+  
+
+
+
   return None
 
 
