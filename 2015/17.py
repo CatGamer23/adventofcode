8747b33416396f36b39481145df99984e5677da9
CatGamer23 <lfiller23@gmail.com>
2024-12-13 02:26:20 -0500

    part 1

--- 2015/17.py
+++ 2015/17.py
@@ -1,5 +1,17 @@
+from rich import print
+import itertools
+
+
 def part1(data: list[str]) -> str | int | float | None:
-  return None
+  container_sizes: list[int] = list(map(int, data))
+
+  valid_combinations: int = 0
+  for i in range(len(container_sizes)):
+    for combination in itertools.combinations(container_sizes, i):
+      if sum(combination) == 150:
+        valid_combinations += 1
+
+  return valid_combinations
 
 
 def part2(data: list[str]) -> str | int | float | None:
