188862614419b595d5b3d77980019b0fb4e8cc02
CatGamer23 <lfiller23@gmail.com>
2025-01-29 16:26:41 -0500

    Changed to run mode

--- main.py
+++ main.py
@@ -176,5 +176,5 @@ def setup() -> None:
 # ------------------------ RUN CODE BELOW ------------------------
 if __name__ == "__main__":
   os.system('cls' if os.name == 'nt' else 'clear')
-  setup()
-  # run()
+  # setup()
+  run()
