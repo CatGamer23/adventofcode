f677daa52a00276b3c5c60d387d0f84c3d75c470
CatGamer23 <lfiller23@gmail.com>
2025-01-29 16:50:50 -0500

    Refactored and part2 added

--- 2015/03.py
+++ 2015/03.py
@@ -1,17 +1,28 @@
-from rich import print
-
 def part1(data: list[str]) -> str | int | float | None:
-  moveindex = {'^': (0, 1), 'v': (0, -1), '>': (1, 0), '<': (-1, 0)}
+  lookupTable = {'^': (0, 1), 'v': (0, -1), '>': (1, 0), '<': (-1, 0)}
   x, y = 0, 0
   visited = {(x, y)}
 
-  for move in data[0]:
-    x += moveindex[move][0]
-    y += moveindex[move][1]
+  for direction in data[0]:
+    x += lookupTable[direction][0]
+    y += lookupTable[direction][1]
     visited.add((x, y))
 
   return len(visited)
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  return None
+  lookupTable = {'^': (0, 1), 'v': (0, -1), '>': (1, 0), '<': (-1, 0)}
+  santa = (0, 0)
+  robot = (0, 0)
+  visited = {santa}
+
+  for i, direction in enumerate(data[0]):
+    if i % 2 == 0:
+      santa = (santa[0] + lookupTable[direction][0], santa[1] + lookupTable[direction][1])
+      visited.add(santa)
+    else:
+      robot = (robot[0] + lookupTable[direction][0], robot[1] + lookupTable[direction][1])
+      visited.add(robot)
+
+  return len(visited)
