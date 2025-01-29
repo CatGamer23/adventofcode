59c9f3ed0401194ede7babcf07b812f37ab6fec5
CatGamer23 <lfiller23@gmail.com>
2025-01-29 16:44:56 -0500

    part1

--- 2015/03.py
+++ 2015/03.py
@@ -1,5 +1,16 @@
+from rich import print
+
 def part1(data: list[str]) -> str | int | float | None:
-  return None
+  moveindex = {'^': (0, 1), 'v': (0, -1), '>': (1, 0), '<': (-1, 0)}
+  x, y = 0, 0
+  visited = {(x, y)}
+
+  for move in data[0]:
+    x += moveindex[move][0]
+    y += moveindex[move][1]
+    visited.add((x, y))
+
+  return len(visited)
 
 
 def part2(data: list[str]) -> str | int | float | None:
