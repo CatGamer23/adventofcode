64e1e4c929db32662b7b96f4c3d51c51f2ac7827
CatGamer23 <lfiller23@gmail.com>
2025-01-29 22:58:34 -0500

    Add type annotations for clarity in part1 function

--- 2015/19.py
+++ 2015/19.py
@@ -2,15 +2,15 @@ from rich import print
 
 
 def part1(data: list[str]) -> str | int | float | None:
-  replacement_rules = data[:-2]
-  starting_molecule = data[-1]
-  unique_molecules = set()
+  replacement_rules: list[str] = data[:-2]
+  starting_molecule: str = data[-1]
+  unique_molecules: set[str] = set()
 
   for rule in replacement_rules:
     input_molecule, output_molecule = rule.split(' => ')
     for i in range(len(starting_molecule)):
       if starting_molecule.startswith(input_molecule, i):
-        new_molecule = starting_molecule[:i] + output_molecule + starting_molecule[i + len(input_molecule):]  # noqa
+        new_molecule: str = starting_molecule[:i] + output_molecule + starting_molecule[i + len(input_molecule):]  # noqa
         unique_molecules.add(new_molecule)
 
   return len(unique_molecules)
