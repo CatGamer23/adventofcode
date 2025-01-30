8603e7f23c6f6a579d2cc96af4d25d8f5bc0a737
CatGamer23 <lfiller23@gmail.com>
2025-01-30 16:10:06 -0500

    added part2 yay

--- 2015/19.py
+++ 2015/19.py
@@ -18,4 +18,16 @@ def part1(data: list[str]) -> str | int | float | None:
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  return None
+  replacement_rules: list[tuple[str, ...]] = [tuple(rule.split(' => ')[::-1]) for rule in data[:-2]]  # noqa
+  molecule_goal: str = data[-1]
+  steps: int = 0
+
+  while molecule_goal != "e":
+    for output_molecule, input_molecule in replacement_rules:
+      if output_molecule in molecule_goal:
+        molecule_goal = molecule_goal.replace(
+          output_molecule, input_molecule, 1)
+        steps += 1
+        break
+
+  return steps
