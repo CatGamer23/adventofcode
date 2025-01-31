80e9c14b41d0af6b573ea1ee6c38cd21761eface
CatGamer23 <lfiller23@gmail.com>
2025-01-31 15:57:30 -0500

    part1

--- 2015/20.py
+++ 2015/20.py
@@ -1,6 +1,18 @@
-from rich import print
+import math
+
+
+def get_divisors(number: int) -> set[int]:
+  return {
+    i for i in range(1, int(math.sqrt(number)) + 1)
+    if number % i == 0
+    for i in (i, number // i)
+  }
+
 
 def part1(data: list[str]) -> str | int | float | None:
+  for i in range(100000, 1000000):
+    if sum(get_divisors(i)) * 10 >= int(data[0]):
+      return i
   return None
 
 
