5050d3dc4f7c62ea998c79ddedf43faa12fffb65
CatGamer23 <lfiller23@gmail.com>
2025-01-31 16:03:40 -0500

    part2

--- 2015/20.py
+++ 2015/20.py
@@ -17,4 +17,7 @@ def part1(data: list[str]) -> str | int | float | None:
 
 
 def part2(data: list[str]) -> str | int | float | None:
+  for i in range(100000, 1000000):
+    if sum(elf for elf in get_divisors(i) if i // elf <= 50) * 11 >= int(data[0]):
+      return i
   return None
