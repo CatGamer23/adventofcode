3f6824f62a6f7925b58825cecc96dd08cb928dff
Cat_Gamer23 <lfiller23@gmail.com>
2025-03-22 02:41:38 -0400

    Added day 21 part 1

--- 2015/21.py
+++ 2015/21.py
@@ -1,4 +1,6 @@
-import json
+from dataclasses import dataclass
+from itertools import combinations, product
+
 from rich import print
 
 
@@ -7,34 +9,41 @@ def part1(data: list[str]) -> str | int | float | None:
   boss_damage: int = int(data[1].split(": ")[1])
   boss_armor: int = int(data[2].split(": ")[1])
   player_hp: int = 100
-  player_damage: int = 0
-  player_armor: int = 0
-  shop: dict[str, list[dict[str, str| int]]] = {
-    "weapons": [
-        {"name": "Dagger", "cost": 8, "damage": 4, "armor": 0},
-        {"name": "Shortsword", "cost": 10, "damage": 5, "armor": 0},
-        {"name": "Warhammer", "cost": 25, "damage": 6, "armor": 0},
-        {"name": "Longsword", "cost": 40, "damage": 7, "armor": 0},
-        {"name": "Greataxe", "cost": 74, "damage": 8, "armor": 0}
-      ],
-    "armor": [
-        {"name": "Leather", "cost": 13, "damage": 0, "armor": 1},
-        {"name": "Chainmail", "cost": 31, "damage": 0, "armor": 2},
-        {"name": "Splintmail", "cost": 53, "damage": 0, "armor": 3},
-        {"name": "Bandedmail", "cost": 75, "damage": 0, "armor": 4},
-        {"name": "Platemail", "cost": 102, "damage": 0, "armor": 5}
-    ],
-    "rings": [
-        {"name": "Damage +1", "cost": 25, "damage": 1, "armor": 0},
-        {"name": "Damage +2", "cost": 50, "damage": 2, "armor": 0},
-        {"name": "Damage +3", "cost": 100, "damage": 3, "armor": 0},
-        {"name": "Defense +1", "cost": 20, "damage": 0, "armor": 1},
-        {"name": "Defense +2", "cost": 40, "damage": 0, "armor": 2},
-        {"name": "Defense +3", "cost": 80, "damage": 0, "armor": 3}
-    ]
-  }
 
-  return None
+  def simulate_battle(player_damage: int, player_armor: int) -> bool:
+    player_turns_to_win: int = -(-boss_hp // max(1, player_damage - boss_armor))  # noqa
+    boss_turns_to_win: int = -(-player_hp // max(1, boss_damage - player_armor))  # noqa
+    return player_turns_to_win <= boss_turns_to_win
+
+  @dataclass
+  class Item:
+    name: str
+    cost: int
+    damage: int
+    armor: int
+
+  items: list[Item] = [
+    # weapons
+    Item("Dagger", 8, 4, 0), Item("Shortsword", 10, 5, 0), Item("Warhammer", 25, 6, 0),
+    Item("Longsword", 40, 7, 0), Item("Greataxe", 74, 8, 0),
+    # armor
+    Item("No Armor", 0, 0, 0), Item("Leather", 13, 0, 1), Item("Chainmail", 31, 0, 2),
+    Item("Splintmail", 53, 0, 3), Item("Bandedmail", 75, 0, 4),
+    # rings
+    Item("No Ring", 0, 0, 0), Item("Damage +1", 25, 1, 0), Item("Damage +2", 50, 2, 0),
+    Item("Damage +3", 100, 3, 0), Item("Defense +1", 20, 0, 1), Item("Defense +2", 40, 0, 2),
+    Item("Defense +3", 80, 0, 3)
+  ]
+
+  min_cost: float = float('inf')
+  for weapon, armor, (ring1, ring2) in product(items[:5], items[5:10], combinations(items[10:], 2)):
+    cost: int = weapon.cost + armor.cost + ring1.cost + ring2.cost
+    damage: int = weapon.damage + ring1.damage + ring2.damage
+    armor_value: int = armor.armor + ring1.armor + ring2.armor
+    if simulate_battle(damage, armor_value):
+      min_cost = min(min_cost, cost)
+
+  return min_cost
 
 
 def part2(data: list[str]) -> str | int | float | None:
