131b8928c92353af0530c10279220c0ae18a8858
CatGamer23 <44417404+CatGamer23@users.noreply.github.com>
2026-02-21 00:37:59 -0500

    Refactor part2 function in 01.py for clarity and consistency; remove redundant variable and improve enumeration

--- 2015/01.py
+++ 2015/01.py
@@ -6,10 +6,9 @@ def part1(data: list[str]) -> str | int | float | None:
 
 
 def part2(data: list[str]) -> str | int | float | None:
-  count, pos = 0, 0
-  for char in data[0]:
+  count: int = 0
+  for pos, char in enumerate(data[0], 1):
     count += 1 if char == '(' else -1
-    pos += 1
     if count <= -1:
       return pos
   return count
