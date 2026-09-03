503530c07ef6b29c43f324f4dd18b1dc785bb9e6
CatGamer23 <44417404+CatGamer23@users.noreply.github.com>
2026-09-03 00:23:36 -0400

    Added corrected template files

---
+++ 2024/08.py
@@ -0,0 +1,8 @@
+from main import Solution
+
+
+def part1(data: list[str]) -> Solution:
+    return None
+
+def part2(data: list[str]) -> Solution:
+    return None
---
+++ 2025/12.py
@@ -0,0 +1,9 @@
+from main import Solution
+
+
+def part1(data: list[str]) -> Solution:
+  return None
+
+
+def part2(data: list[str]) -> Solution:
+  return None
